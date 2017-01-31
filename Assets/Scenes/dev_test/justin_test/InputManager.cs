55b405be225321a49c1a417c57a0192321f54902
JUSTINSLAPTOP\Justin <jwcain@mtu.edu>
2017-01-30 20:59:32 -0500

    Starting testing some simple networking.

---
+++ Assets/Scenes/dev_test/justin_test/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	static MultiplayerInputMapper player1;
+	static MultiplayerInputMapper player2;
+
+	//Returns true if the requesting client was able to register a new controller for the game
+	static bool RegisterController(MultiplayerInputMapper controller) {
+		if (player1 == null) {
+			player1 = controller;
+			return true;
+		}
+		else if (player2 == null) {
+			player2 = controller;
+			return true;
+
+		}
+
+		return false;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scenes/dev_test/justin_test/MultiplayerInputMapper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class MultiplayerInputMapper : NetworkBehaviour {
+
+	public override void OnStartLocalPlayer() {
+		this.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Do not run this method on this client if it is not our local player object
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		string button = "Test";
+		if (Input.GetButtonDown(button)) {
+			Debug.Log("Click");
+			Vector3 rng = new Vector3(Random.value, Random.value, Random.value);
+			this.transform.position = rng;
+		}
+
+	}
+}
